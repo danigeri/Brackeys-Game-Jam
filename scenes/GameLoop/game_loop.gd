8b519e0aeddff8f15e5af8707fbfb6315c9ed872
Balazs <balzs.gyngysi@gmail.com>
2026-02-17 23:08:22 +0100

    act1 level + start dots

--- scenes/GameLoop/game_loop.gd
+++ scenes/GameLoop/game_loop.gd
@@ -2,8 +2,14 @@ extends Node2D
 
 var pressed_cursor: Texture2D
 var default_custom_cursor: Texture2D
-@onready var ghost_camera: Camera2D = $GhostCamera
+var positions = []
+var record_timer: float = 0.0
+var ghost_mode = false
+const RECORD_INTERVAL := 0.3 # 1 second
 
+@onready var ghost_camera: Camera2D = $GhostCamera
+@onready var player: CharacterBody2D = $Player
+@onready var line_2d: Line2D = $Line2D
 
 func _ready() -> void:
 	pressed_cursor = preload("uid://cbdwnan67004a")
@@ -20,16 +26,33 @@ func _input(event: InputEvent) -> void:
 		else:
 			Input.set_custom_mouse_cursor(default_custom_cursor)
 
+#kell majd a linehoz
+#func _process(delta: float) -> void:
+	#record_timer += delta
+	#if record_timer >= RECORD_INTERVAL:
+		#record_timer -= RECORD_INTERVAL
+	#if !ghost_mode:
+		#positions.append(player.global_position)
 
 func handle_ghost_mode(is_ghost_mode) -> void:
+	#print("fhost mode signal: ", is_ghost_mode)
+	#ghost_mode = is_ghost_mode
 	use_ghost_camera(is_ghost_mode)
 	show_hide_cursor(is_ghost_mode)
 
-
 func use_ghost_camera(is_ghost_mode) -> void:
 	if is_ghost_mode:
+		#add_position()
 		ghost_camera.make_current()
+	#else:
+	#	line_2d.clear_points()
 
+func clear_line_positions() -> void:
+	line_2d.clear_points()
+
+func add_position() -> void:
+	for pos in positions:
+		line_2d.add_point(pos)
 
 func show_hide_cursor(is_ghost_mode):
 	if is_ghost_mode:
@@ -37,3 +60,4 @@ func show_hide_cursor(is_ghost_mode):
 		Input.warp_mouse(get_viewport().get_visible_rect().size / 2)
 	else:
 		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+		
