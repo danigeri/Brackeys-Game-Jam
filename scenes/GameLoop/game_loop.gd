04c16ed45c72b7aa7381a888c1e81d96ed7c6cb4
nagyrichi <30602658+nagyrichi@users.noreply.github.com>
2026-02-18 08:15:33 +0100

    optional and required stars, last required will trigger the ghost run

--- scenes/GameLoop/game_loop.gd
+++ scenes/GameLoop/game_loop.gd
@@ -2,14 +2,51 @@ extends Node2D
 
 @onready var ghost_camera: Camera2D = $GhostCamera
 
+var required_total : int =  0
+var optional_total : int =  0
+var required_collected : int =  0
+var optional_collected : int =  0
 
 func _ready() -> void:
 	print("ready")
 	#MusicPlayer.start_music()
-	GameEvents.ghost_mode_on.connect(use_ghost_camera)
-	#GameEvents.ghost_mode_on.emit(false)
+	GameEvents.ghost_mode_on.connect(ghost_mode_triggered)
+	
+	for star in get_tree().get_nodes_in_group("stars"):
+		star.collected.connect(_on_star_collected)
+		if star.star_type == star.StarType.REQUIRED:
+			required_total += 1
+		elif star.star_type == star.StarType.OPTIONAL:
+			optional_total += 1
+	
+	print("ready, required_total: " , required_total)
+	print("ready, optional_total: " , optional_total)
 
-
-func use_ghost_camera(value) -> void:
+func ghost_mode_triggered(value) -> void:
 	if value:
 		ghost_camera.make_current()
+		
+	required_collected = 0
+	optional_collected = 0 
+	for star in get_tree().get_nodes_in_group("stars"):
+		star.reset_star()
+		print("star reset: " , star)
+		
+	print("ready, required_collected: " , required_collected)
+	print("ready, optional_collected: " , optional_collected)
+		
+func _on_star_collected(star):
+	if star.star_type == star.StarType.REQUIRED:
+		required_collected += 1
+	elif star.star_type == star.StarType.OPTIONAL:
+		optional_collected += 1
+		
+	
+	print("ready, required_collected: " , required_collected)
+	print("ready, optional_collected: " , optional_collected)
+
+	if required_collected >= required_total:
+		call_deferred("_trigger_ghost_mode")
+
+func _trigger_ghost_mode():
+	GameEvents.ghost_mode_on.emit(true)
--- scenes/Star/star.gd
+++ scenes/Star/star.gd
@@ -1,6 +1,32 @@
 extends Area2D
 
+signal collected(star)
 
-func _on_body_entered(body: Node2D) -> void:
-	print("COIN reached by : ", body)
-	GameEvents.ghost_mode_on.emit(true)
+enum StarType { OPTIONAL, REQUIRED }
+
+var collected_once : bool = false
+
+@export var star_type : StarType = StarType.OPTIONAL
+
+func _ready():
+	add_to_group("stars")
+
+func _on_body_entered(body):
+	print("COLLISION: ", body.is_in_group("Player"))
+	if body.is_in_group("Player"):
+		if collected_once:
+			return
+		
+		print("STAR reached by : ", body, ", star: " , self)
+		if body is CharacterBody2D:
+			collected_once = true
+			collected.emit(self)
+			hide()
+			$CollisionShape2D.disabled = true
+			#queue_free()
+		
+func reset_star():
+	print("STAR RESET :" , self)
+	collected_once = false
+	show()
+	$CollisionShape2D.disabled = false
