c13f722477a19a1c3e6cba59855c9ed3c2fe91f8
Gergő Dániel <danigerci@gmail.com>
2026-02-18 19:32:36 +0100

    Sound player prepared for multiple parallel sounds..

--- Globals/SoundManager/sound_manager.gd
+++ Globals/SoundManager/sound_manager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @export var click_sound: AudioStream
+@export var platform_move_sound: AudioStream
 
 @onready var sfx_player: AudioStreamPlayer = $SfxPlayer
 
@@ -8,12 +9,33 @@ extends Node
 func _ready() -> void:
 	sfx_player.max_polyphony = 4
 
+# Dictionary to track which streams are currently playing
+var active_sounds = {}
+
 
 func play_sfx(stream: AudioStream) -> AudioStreamPlayer:
-	sfx_player.stream = stream
-	sfx_player.play()
-	return sfx_player
+	if active_sounds.has(stream) and active_sounds[stream].is_playing():
+		return active_sounds[stream]
+
+	var new_player = AudioStreamPlayer.new()
+	add_child(new_player)
+	
+	new_player.stream = stream
+	new_player.play()
+	
+	active_sounds[stream] = new_player
+	
+	new_player.finished.connect(func():
+		active_sounds.erase(stream)
+		new_player.queue_free()
+	)
+	
+	return new_player
 
 
 func play_click_sound() -> AudioStreamPlayer:
 	return play_sfx(click_sound)
+
+
+func play_platform_move() -> AudioStreamPlayer:
+	return play_sfx(platform_move_sound)
