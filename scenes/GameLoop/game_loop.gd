bb61d2e5506c203fed026f4eed77b3bd379f11aa
nagyrichi <30602658+nagyrichi@users.noreply.github.com>
2026-02-18 08:33:46 +0100

    gdformat + gdlint

--- scenes/GameLoop/game_loop.gd
+++ scenes/GameLoop/game_loop.gd
@@ -2,12 +2,13 @@ extends Node2D
 
 var pressed_cursor: Texture2D
 var default_custom_cursor: Texture2D
+var required_total: int = 0
+var optional_total: int = 0
+var required_collected: int = 0
+var optional_collected: int = 0
+
 @onready var ghost_camera: Camera2D = $GhostCamera
 
-var required_total : int =  0
-var optional_total : int =  0
-var required_collected : int =  0
-var optional_collected : int =  0
 
 func _ready() -> void:
 	pressed_cursor = preload("uid://cbdwnan67004a")
@@ -15,16 +16,16 @@ func _ready() -> void:
 	#MusicPlayer.start_music()
 	GameEvents.ghost_mode_on.connect(handle_ghost_mode)
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	
+
 	for star in get_tree().get_nodes_in_group("stars"):
 		star.collected.connect(_on_star_collected)
 		if star.star_type == star.StarType.REQUIRED:
 			required_total += 1
 		elif star.star_type == star.StarType.OPTIONAL:
 			optional_total += 1
-	
-	print("ready, required_total: " , required_total)
-	print("ready, optional_total: " , optional_total)
+
+	print("ready, required_total: ", required_total)
+	print("ready, optional_total: ", optional_total)
 
 
 func _input(event: InputEvent) -> void:
@@ -40,14 +41,14 @@ func handle_ghost_mode(is_ghost_mode) -> void:
 	show_hide_cursor(is_ghost_mode)
 	reset_stars(is_ghost_mode)
 
-	
+
 func reset_stars(is_ghost_mode) -> void:
 	if is_ghost_mode:
 		required_collected = 0
 		optional_collected = 0
 		for star in get_tree().get_nodes_in_group("stars"):
 			star.reset_star()
-			print("star reset: " , star)
+			print("star reset: ", star)
 
 
 func use_ghost_camera(is_ghost_mode) -> void:
@@ -68,17 +69,18 @@ func _on_star_collected(star):
 		required_collected += 1
 	elif star.star_type == star.StarType.OPTIONAL:
 		optional_collected += 1
-		
-	
-	print("ready, required_collected: " , required_collected)
-	print("ready, optional_collected: " , optional_collected)
+
+	print("ready, required_collected: ", required_collected)
+	print("ready, optional_collected: ", optional_collected)
 
 	if required_collected >= required_total:
 		call_deferred("_trigger_ghost_mode")
 
-#ez azert kell, mert idozites miatt nem rajzolodott vissza ghost mode-ban 
+
+#ez azert kell, mert idozites miatt nem rajzolodott vissza ghost mode-ban
 #az utoljara felszedett star, csak az osszes tobbi
 #ha van valami jobb megoldas akkor javitsuk
+
 
 func _trigger_ghost_mode():
 	GameEvents.ghost_mode_on.emit(true)
