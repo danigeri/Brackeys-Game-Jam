2ac8c15bc3b4472d165c5d6b5410e4828cfa5a29
Gergő Dániel <danigerci@gmail.com>
2026-02-20 13:25:33 +0100

    MainMenu musis added

--- UI/Menus/MainMenu/main_menu.gd
+++ UI/Menus/MainMenu/main_menu.gd
@@ -1,5 +1,10 @@
 extends Control
 
+const GAME_LOOP = "uid://cp03gm3dbew5m"
+
+func _ready() -> void:
+	MusicPlayer.fade_in_music()
+
 
 func _on_exit_button_pressed() -> void:
 	await SoundManager.play_sound_by_id(SoundManager.Sound.CLICK).finished
@@ -8,4 +13,10 @@ func _on_exit_button_pressed() -> void:
 
 func _on_start_button_pressed() -> void:
 	await SoundManager.play_sound_by_id(SoundManager.Sound.CLICK).finished
-	get_tree().change_scene_to_file("res://game_loop.tscn")
+	
+	#TODO: Add curtain transition
+	MusicPlayer.fade_out_music(1.5)
+	await get_tree().create_timer(1.5).timeout
+	
+	
+	get_tree().change_scene_to_file(GAME_LOOP)
