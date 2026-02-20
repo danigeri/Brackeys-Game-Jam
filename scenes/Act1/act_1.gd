b6cce4450514f99c15786a951dbb79b07f0a5e72
Balazs <balzs.gyngysi@gmail.com>
2026-02-20 18:28:16 +0100

    bg fix

--- scenes/Act1/act_1.gd
+++ scenes/Act1/act_1.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var environment: Node2D = $Environment
 @onready var ghost_background: Sprite2D = $GhostBackground
+@onready var ghost_background_layer_2: Sprite2D = $GhostBackgroundLayer2
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,3 +13,4 @@ func _ready() -> void:
 func _handle_ghost_mode_on(is_on: bool):
 	environment.visible = not is_on
 	ghost_background.visible = is_on
+	ghost_background_layer_2.visible = is_on
