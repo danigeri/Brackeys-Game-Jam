86e3bf1a2a6e80452820f6924f13e645248fea2a
Balazs <balzs.gyngysi@gmail.com>
2026-02-22 01:14:34 +0100

    add temp level changer

---
+++ scenes/GameLoop/temp_level_picker.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _on_button_pressed() -> void:
+	GameEvents.change_act_to(1)
+	GameEvents._set_ghost_mode(false)
+	
+
+func _on_button_2_pressed() -> void:
+		GameEvents.change_act_to(2)
+		GameEvents._set_ghost_mode(false)
+
+
+func _on_button_3_pressed() -> void:
+		GameEvents.change_act_to(3)
+		GameEvents._set_ghost_mode(false)
