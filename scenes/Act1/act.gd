4ad05cf0c7dd5c94241f932525d575f1ccca5b83
Gergő Dániel <danigerci@gmail.com>
2026-02-22 10:07:49 +0100

    Act3 background fixed.

--- scenes/Act1/act.gd
+++ scenes/Act1/act.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
 @onready var environment = $Environment
-@onready var ghost_background: Sprite2D = $GhostBackground
-@onready var ghost_background_layer_2: Sprite2D = $GhostBackgroundLayer2
+@onready var ghost_background: Node2D = $GhostBackground
 @onready var curtain_ghost: Sprite2D = $CurtainGhost
 @onready var tutorial_cursor: Node2D = $TutorialCursor
 @onready var curtain: Sprite2D = $CanvasLayer/Curtain
@@ -18,7 +17,6 @@ func _handle_ghost_mode_on(is_on: bool):
 	handle_tutorial()
 	environment.visible = not is_on
 	ghost_background.visible = is_on
-	ghost_background_layer_2.visible = is_on
 	curtain.visible = !is_on
 	curtain_ghost.visible = is_on
 
