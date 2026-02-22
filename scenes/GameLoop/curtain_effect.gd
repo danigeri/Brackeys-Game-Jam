1b7f17107af8ce6d5fe93fc820312d72d1c21b01
Balazs <balzs.gyngysi@gmail.com>
2026-02-22 10:37:40 +0100

    font and title

--- scenes/GameLoop/curtain_effect.gd
+++ scenes/GameLoop/curtain_effect.gd
@@ -1,6 +1,7 @@
 extends Sprite2D
 
 @onready var label: Label = $Label
+@onready var subtitle: Label = $Subitle
 
 
 func set_act_number(number: int) -> void:
@@ -8,3 +9,11 @@ func set_act_number(number: int) -> void:
 	for i in range(number):
 		act_text += "I"
 	label.text = "Act " + str(act_text)
+	var new_subtitle = "Firenze"
+	match number:
+		1: new_subtitle = "Firenze"
+		2: new_subtitle = "Inferno"
+		3: new_subtitle = "Paradiso"
+	subtitle.text = new_subtitle
+	
+	
